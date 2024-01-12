cc4b19a9459e5ca0113ebc6f07ca87fd212be13e
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-01-12 20:11:33 +0200

    Relocate User Registration action from `UserController` into `AuthController`

--- TravelAccommodationBooking.API/Controllers/AuthenticationController.cs
+++ TravelAccommodationBooking.API/Controllers/AuthenticationController.cs
@@ -6,6 +6,7 @@ using TravelAccommodationBooking.API.Exceptions;
 using TravelAccommodationBooking.API.Models.Error;
 using TravelAccommodationBooking.API.Models.Login;
 using TravelAccommodationBooking.API.Models.User;
+using TravelAccommodationBooking.API.Services;
 using TravelAccommodationBooking.API.Services.Interfaces;
 using TravelAccommodationBooking.Db.Utilities.Exceptions;
 
@@ -16,17 +17,24 @@ namespace TravelAccommodationBooking.API.Controllers
     public class AuthenticationController : ControllerBase
     {
         private readonly IAuthenticationService _authenticationService;
+        private readonly IUserService _userService;
         private readonly IValidator<LoginRequest> _loginValidator;
+        private readonly IValidator<UserRequest> _userValidator;
         private readonly IMapper _mapper;
 
         public AuthenticationController(
             IAuthenticationService authenticationService,
+            IUserService userService,
             IValidator<LoginRequest> loginValidator,
+            IValidator<UserRequest> userValidator,
             IMapper mapper
             )
         {
-            _authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
-            _loginValidator = loginValidator ?? throw new ArgumentNullException(nameof(loginValidator)); 
+            _authenticationService = authenticationService;
+            _userService = userService;
+            _loginValidator = loginValidator ?? throw new ArgumentNullException(nameof(loginValidator));
+            _userValidator = userValidator
+                ?? throw new ArgumentNullException(nameof(userValidator));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
@@ -73,5 +81,47 @@ namespace TravelAccommodationBooking.API.Controllers
                 });
             }
         }
+
+        /// <summary>
+        /// Register a new user.
+        /// </summary>
+        /// <param name="userRequest">User registration data.</param>
+        /// <returns>A response indicating the result of the user registration process.</returns>
+        [HttpPost("register")]
+        [SwaggerResponse(StatusCodes.Status200OK, "User registered successfully.", typeof(UserResponse))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid request data", typeof(IEnumerable<string>))]
+        [SwaggerResponse(StatusCodes.Status409Conflict, "Username or email already exists", typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error occurred", typeof(ErrorResponse))]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRequest userRequest)
+        {
+            var validationResult = _userValidator.Validate(userRequest);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
+            try
+            {
+                var registeredUser = await _userService.RegisterUserAsync(userRequest);
+                return Ok(new { Message = "User registered successfully.", User = registeredUser });
+            }
+            catch (DuplicateUsernameException ex)
+            {
+                return Conflict(new ErrorResponse { Message = ex.Message });
+            }
+            catch (DuplicateEmailException ex)
+            {
+                return Conflict(new ErrorResponse { Message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ErrorResponse
+                {
+                    Message = "An error occurred while registering the user.",
+                    Error = ex.Message
+                });
+            }
+        }
+
     }
 }
--- TravelAccommodationBooking.API/Controllers/UserController.cs
+++ TravelAccommodationBooking.API/Controllers/UserController.cs
@@ -1,10 +1,8 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Swashbuckle.AspNetCore.Annotations;
 using TravelAccommodationBooking.API.Models.Error;
 using TravelAccommodationBooking.API.Models.User;
 using TravelAccommodationBooking.API.Services;
-using TravelAccommodationBooking.Db.Utilities.Exceptions;
 
 namespace TravelAccommodationBooking.API.Controllers
 {
@@ -13,54 +11,14 @@ namespace TravelAccommodationBooking.API.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
-        private readonly IValidator<UserRequest> _userValidator;
+        private readonly IMapper _mapper;
 
-        public UserController(IUserService userService, IValidator<UserRequest> userValidator)
+        public UserController(
+            IUserService userService,
+            )
         {
             _userService = userService;
-            _userValidator = userValidator
-                ?? throw new ArgumentNullException(nameof(userValidator));
-        }
-
-        /// <summary>
-        /// Register a new user.
-        /// </summary>
-        /// <param name="userRequest">User registration data.</param>
-        /// <returns>A response indicating the result of the user registration process.</returns>
-        [HttpPost("register")]
-        [SwaggerResponse(StatusCodes.Status200OK, "User registered successfully.", typeof(UserResponse))]
-        [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid request data", typeof(IEnumerable<string>))]
-        [SwaggerResponse(StatusCodes.Status409Conflict, "Username or email already exists", typeof(ErrorResponse))]
-        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Internal server error occurred", typeof(ErrorResponse))]
-        public async Task<IActionResult> RegisterUser([FromBody] UserRequest userRequest)
-        {
-            var validationResult = _userValidator.Validate(userRequest);
-            if (!validationResult.IsValid)
-            {
-                return BadRequest(validationResult.Errors);
-            }
-
-            try
-            {
-                var registeredUser = await _userService.RegisterUserAsync(userRequest);
-                return Ok(new { Message = "User registered successfully.", User = registeredUser });
-            }
-            catch (DuplicateUsernameException ex)
-            {
-                return Conflict(new ErrorResponse { Message = ex.Message });
-            }
-            catch (DuplicateEmailException ex)
-            {
-                return Conflict(new ErrorResponse { Message = ex.Message });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ErrorResponse
-                {
-                    Message = "An error occurred while registering the user.",
-                    Error = ex.Message
-                });
-            }
+            _mapper = mapper;
         }
     }
 }
