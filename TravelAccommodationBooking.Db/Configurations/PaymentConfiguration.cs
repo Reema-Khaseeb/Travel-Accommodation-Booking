ad0cb0e1e89743392f8809840a4ef16d8d98a4ce
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-02-14 00:28:36 +0200

    Resolve merge conflict

---
+++ TravelAccommodationBooking.Db/Configurations/PaymentConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using TravelAccommodationBooking.Db.Models;
+
+namespace TravelAccommodationBooking.Db.Configurations
+{
+    public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            // Primary Key
+            builder.HasKey(b => b.PaymentId);
+
+            // Properties
+            builder.Property(p => p.PaymentId)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(p => p.Amount)
+                .HasColumnType("decimal(10, 2)")
+                .IsRequired();
+            
+            builder.Property(p => p.PaymentDate)
+                .HasColumnType("decimal(10, 2)")
+                .IsRequired();
+
+            builder.Property(p => p.Status)
+                .HasColumnType("decimal(10, 2)")
+                .IsRequired();
+
+            // Relationships and Constraints
+            builder.HasOne(p => p.Booking)
+                .WithOne(b => b.Payment)
+                .HasForeignKey<Payment>(p => p.BookingId);
+
+            // Table Name in Database
+            builder.ToTable("Payment");
+        }
+    }
+}
---
+++ TravelAccommodationBooking.Db/Models/Payment.cs
@@ -0,0 +1,17 @@
+﻿using TravelAccommodationBooking.Db.Utilities.Enums;
+namespace TravelAccommodationBooking.Db.Models;
+
+public class Payment
+{
+    public int PaymentId { get; set; }
+    public decimal Amount { get; set; }
+    public bool IsProcessed { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public PaymentStatus Status { get; set; }
+    
+    // Foreign key
+    public int BookingId { get; set; }
+
+    // Navigation Property
+    public Booking Booking { get; set; }
+}
