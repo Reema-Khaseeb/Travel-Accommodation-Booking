3eec88c296fb21445449bb3ae130ebc10212d7cc
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-02-13 20:53:51 +0200

    Add `Reviews` to `User` Model

--- TravelAccommodationBooking.Db/Models/User.cs
+++ TravelAccommodationBooking.Db/Models/User.cs
@@ -7,7 +7,10 @@ namespace TravelAccommodationBooking.Db.Models
         public int UserId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public UserRole Role { get; set; } = UserRole.User;
+        public UserRole Role { get; set; }
         public string Email { get; set; }
+
+        // Navigation properties
+        public IEnumerable<Review> Reviews { get; set; }
     }
 }
