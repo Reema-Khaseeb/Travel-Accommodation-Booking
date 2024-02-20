93c7e4d203fd820ca1b680af31b05f1b2ed6c450
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-02-20 06:11:43 +0200

    Create `RoomRespository` CRUD operations wth interface

---
+++ TravelAccommodationBooking.Db/Repositories/Interfaces/IRoomRepository.cs
@@ -0,0 +1,13 @@
+﻿using TravelAccommodationBooking.Db.Models;
+
+namespace TravelAccommodationBooking.Db.Repositories.Interfaces
+{
+    public interface IRoomRepository
+    {
+        Task<Room> CreateRoomAsync(Room room);
+        Task UpdateRoomAsync(Room room);
+        Task DeleteRoomAsync(Room room);
+        Task<Room> GetRoomByIdAsync(int roomId);
+        Task<IEnumerable<Room>> GetRoomsAsync();
+    }
+}
---
+++ TravelAccommodationBooking.Db/Repositories/RoomRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAccommodationBooking.Db.Models;
+using TravelAccommodationBooking.Db.Repositories.Interfaces;
+
+namespace TravelAccommodationBooking.Db.Repositories
+{
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly TravelAccommodationBookingDbContext _context;
+
+        public RoomRepository(TravelAccommodationBookingDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Room> CreateRoomAsync(Room room)
+        {
+            await _context.Rooms.AddAsync(room);
+            await _context.SaveChangesAsync();
+            return room;
+        }
+
+        public async Task UpdateRoomAsync(Room room)
+        {
+            _context.Rooms.Update(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteRoomAsync(Room room)
+        {
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Room>> GetRoomsAsync()
+        {
+            return await _context.Rooms.ToListAsync();
+        }
+
+        public async Task<Room> GetRoomByIdAsync(int roomId)
+        {
+            return await _context.Rooms.FindAsync(roomId);
+        }
+    }
+}
