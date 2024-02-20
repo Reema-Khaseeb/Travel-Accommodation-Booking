639873967a692c22f5db18ddd097ff68cf2e952f
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-02-20 06:13:42 +0200

    Create `CityRespository` CRUD operations wth interface

---
+++ TravelAccommodationBooking.Db/Repositories/Interfaces/ICityRepository.cs
@@ -0,0 +1,13 @@
+﻿using TravelAccommodationBooking.Db.Models;
+
+namespace TravelAccommodationBooking.Db.Repositories.Interfaces
+{
+    public interface ICityRepository
+    {
+        Task<City> CreateCityAsync(City city);
+        Task UpdateCityAsync(City city);
+        Task DeleteCityAsync(City city);
+        Task<City> GetCityByIdAsync(int cityId);
+        Task<IEnumerable<City>> GetCitiesAsync();
+    }
+}
