be396359e96f319733c61b86805e80bd04293aad
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-02-24 20:35:53 +0200

    Adjust `UpdateHotelAsync` to handle FKs in Hotel when updated

--- TravelAccommodationBooking.API/Services/HotelService.cs
+++ TravelAccommodationBooking.API/Services/HotelService.cs
@@ -14,16 +14,22 @@ namespace TravelAccommodationBooking.API.Services
     public class HotelService : IHotelService
     {
         private readonly IHotelRepository _hotelRepository;
+        private readonly ICityService _cityService;
+        private readonly IHotelLocationService _hotelLocationService;
         private readonly ILogger<HotelService> _logger;
         private readonly IMapper _mapper;
 
         public HotelService(
             IHotelRepository hotelRepository,
+            ICityService cityService,
+            IHotelLocationService hotelLocationService,
             ILogger<HotelService> logger,
             IMapper mapper
             )
         {
             _hotelRepository = hotelRepository;
+            _cityService = cityService;
+            _hotelLocationService = hotelLocationService;
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _mapper = mapper;
         }
@@ -35,14 +41,26 @@ namespace TravelAccommodationBooking.API.Services
             return newHotel;
         }
 
-        public async Task UpdateHotelAsync(int hotelId, HotelUpdateRequest hotelRequest)
+        public async Task UpdateHotelAsync(int hotelId, HotelUpdateRequest hotelUpdateRequest)
         {
             var existedHotel = await _hotelRepository.GetHotelByIdAsync(hotelId);
+            _mapper.Map(hotelUpdateRequest, existedHotel);
 
-            _mapper.Map(hotelRequest, existedHotel);
+            // Validate and update foreign keys
+            await UpdateCityForHotelAsync(existedHotel, hotelUpdateRequest.CityId);
+            await UpdateLocationForHotelAsync(existedHotel, hotelUpdateRequest.LocationId);
+
             existedHotel.ModificationDate = DateTime.UtcNow;
+
+            try
+            {
             await _hotelRepository.UpdateHotelAsync(existedHotel);
             _logger.LogInformation($"Hotel-{existedHotel.HotelId} updated successfully.");
+        }
+            catch (Exception ex)
+            {
+                throw new DatabaseUpdateException("An error occurred while updating the hotel.", ex);
+            }
         }
 
         public async Task DeleteHotelAsync(int hotelId)
@@ -70,6 +88,40 @@ namespace TravelAccommodationBooking.API.Services
             var hotels = await _hotelRepository.GetHotelsAsync();
             _logger.LogInformation("All Hotels successfully retrieved.");
             return hotels;
+        }
+
+        private async Task UpdateCityForHotelAsync(Hotel existingHotel, int? cityId)
+        {
+            if (cityId.HasValue)
+            {
+                try
+                {
+                    var existingCity = await _cityService.GetCityByIdAsync(cityId.Value);
+                    existingHotel.City = existingCity;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error while validating and setting City for the updated hotel.");
+                    throw;
+                }
+            }
+        }
+
+        private async Task UpdateLocationForHotelAsync(Hotel existingHotel, int? locationId)
+        {
+            if (locationId.HasValue)
+            {
+                try
+                {
+                    var existingLocation = await _hotelLocationService.GetLocationByIdAsync(locationId.Value);
+                    existingHotel.Location = existingLocation;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error while validating and setting Location for the updated hotel.");
+                    throw;
+                }
+            }
         }
     }
 }
