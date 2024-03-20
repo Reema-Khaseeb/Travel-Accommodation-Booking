8eef96f2f88f2ac1743f1dc10251e5aadb25dd22
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-03-20 05:27:46 +0200

    Restructure: Relocate `Services/` and their interfaces into Business Logic Layer

---
+++ TravelAccommodationBooking/Services/Interfaces/ICityService.cs
@@ -0,0 +1,15 @@
+﻿using TravelAccommodationBooking.Dtos.City;
+using TravelAccommodationBooking.Db.Models;
+
+namespace TravelAccommodationBooking.Services.Interfaces;
+
+public interface ICityService
+{
+    Task<City> CreateCityAsync(City city);
+    Task UpdateCityAsync(int cityId, CityAdminRequest cityRequest);
+    Task DeleteCityAsync(int cityId);
+    Task<IEnumerable<City>> GetCitiesAsync();
+    Task<IEnumerable<CityWithHotelsCountView>> GetCitiesWithHotelsCountViewAsync();
+    Task<CityWithHotelsCountView> GetCityByIdWithHotelsCountViewAsync(int cityId);
+    Task<City> GetCityByIdAsync(int cityId);
+}
---
+++ TravelAccommodationBooking/Services/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using TravelAccommodationBooking.Db.Models;
+
+namespace TravelAccommodationBooking.Services.Interfaces;
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(User user);
+    bool ValidateToken(string token);
+}
